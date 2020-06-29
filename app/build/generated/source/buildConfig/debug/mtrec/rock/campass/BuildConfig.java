a63b417ee4ac5fd0afaed9cd1426995fb0855250
Dingdong-LIU <dliuak@connect.ust.hk>
2020-06-29 15:13:40 +0800

    initial set up

---
+++ app/build/generated/source/buildConfig/debug/mtrec/rock/campass/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package mtrec.rock.campass;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "mtrec.rock.campass";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
